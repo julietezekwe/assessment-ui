3c59b492394da38e67c387e86bc06baf61bdf449
MAC <mac@MACs-MacBook-Pro.local>
2020-04-05 11:25:57 +0100

    feat[profile]: populate profile page [Finishes #123354667]

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,13 @@ import 'semantic-ui-css/semantic.min.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Body from '../../components/Body';
+import Profile from '../../components/Profile';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Profile />
       <Body />
       <Footer />
     </>
